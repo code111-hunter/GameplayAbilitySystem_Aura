371fd58eef19b05c0c5ea4ffa78f1342a597f05e
code111-hunter <82190795+code111-hunter@users.noreply.github.com>
2024-06-22 16:54:58 +0800

    Create PlayerController

---
+++ Source/GAS_Aura/Public/Character/AuraCharacter.h
@@ -0,0 +1,17 @@
+// Copyright Aura
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraBaseCharacter.h"
+#include "AuraCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_API AAuraCharacter : public AAuraBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GAS_Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,17 @@
+// Copyright Aura
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraBaseCharacter.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_API AAuraEnemy : public AAuraBaseCharacter
+{
+	GENERATED_BODY()
+	
+};
